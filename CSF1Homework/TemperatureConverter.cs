6291fc30e7ef57568d5192eda7ad7cca72c7d5f0
LynnDCoding@outlook.com <LynnDCoding@outlook.com>
2021-09-27 11:13:39 -0500

    Adding Temperature Converter Changes

--- CSF1Homework/TemperatureConverter.cs
+++ CSF1Homework/TemperatureConverter.cs
@@ -10,7 +10,6 @@ namespace CSF1Homework
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("CSF1 Homework: Temperature Converter");
             Console.Title = "-====== CSF1 Homework: Temperature Converter ======-";//names the Console Window
 
@@ -22,38 +21,33 @@ namespace CSF1Homework
             double fahrenheitTemp = double.Parse(Console.ReadLine());
             double fahrTocels = (fahrenheitTemp - 32) / (9.0 / 5.0);
 
-            //THIS SECTION IS FOR THE MENU: ONLY 2 CHOICES
-            //int userInput = Console.ReadLine();
-            //ConsoleKey userInput = Console.ReadKey(true).Key;
+            Console.WriteLine("Would you like to see the conversions? Y/N:");
+            ConsoleKey runProgram = Console.ReadKey(true).Key; //COUNTER
 
-
-            //Do While ALWAYS runs once. It's BEST for menus!
-            Console.WriteLine("Please select from the following menu:");
-            string userInput = Console.ReadLine().ToLower();
-
-            //bool repeat = true;//They want to use our program.
-
-            do
+            while (runProgram == ConsoleKey.Y)
             {
-                Console.WriteLine("A) Convert Celsius to Fahrenheit\nB) Convert Fahrenheit to Celsius\n");
-                switch (userInput)
+                runProgram = Console.ReadKey(true).Key;
+                Console.WriteLine("Please select from the following options:\nA) Convert Celsius to Fahrenheit\n B) Convert Fahrenheit to Celsius\n");
+                string menuChoice = Console.ReadLine().ToLower();
+
+                switch (menuChoice)
                 {
                     case "a":
-                        Console.WriteLine("A) Convert Celsius to Fahrenheit\n\n");
+                        Console.WriteLine(" You entered " + celsiusTemp + " degrees celsius." +
+                           "This converts to a temperature of " + celsTofahr + " degrees Fahrenheit\n\n");
                         break;
 
                     case "b":
                         Console.WriteLine("B) Convert Fahrenheit to Celsius\n\n");
-                        //stop the loop
-                        //(userInput != " ");//UPDATE
+                        Console.WriteLine(" You entered " + fahrenheitTemp + " degrees fahrenheit." +
+                           "This converts to a temperature of " + fahrTocels + "degrees celsius\n\n");
                         break;
+
                     default:
-                        Console.WriteLine("That was not a valid option.\n ");
+                        Console.WriteLine("That was not a valid option. ");
                         break;
                 }//end switch
-
-            } while (userInput != " ");//repeat is the CONDITION.
-
+            }//end while
         }//end Main()
     }//end class
 }//end namespace
